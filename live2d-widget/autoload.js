fe1e56e1781725ef495713113a7097ad8b876d81
wangwei <1292627970@qq.com>
2020-08-31 16:11:35 +0800

    Site updated: 2020-08-31 16:11:32

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,4 @@
-const live2d_path = "/live2d-widget/";
-//const live2d_path = "./";
+const live2d_path = "/blog/live2d-widget/";
 
 $("<link>").attr({href: live2d_path + "waifu.css", rel: "stylesheet", type: "text/css"}).appendTo("head");
 //waifu.css的绝对路径
